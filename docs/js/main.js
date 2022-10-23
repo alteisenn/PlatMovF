182613f52e5c3ab4c693e93ae8f4b33ea08a4ee9
ELmuri <101751763+Elmuri@users.noreply.github.com>
2022-10-23 17:23:32 -0300

    básica extracción de datos
    ahora muestra los detalles en una modal, el objetivo optimo seria realizarlo con jQuery para realizar las modificaciones al DOM mas fácilmente.

--- docs/js/main.js
+++ docs/js/main.js
@@ -11,28 +11,48 @@ const conteinerTV = document.getElementById("conteienerTV");
 let i = 2;
 let tv_id = 2;
 // NOTE - obteniendo las peliculas ejemplo
-function cargarMasMovi() {
+function MoreMovi() {
     i++;
-    CargarMovi(i);
+    getMovi(i);
 }
-function CargarMovi(i) {
+function getMovi(i) {
     fetch(`${baseURL}movie/${i}?api_key=${API_KEY}&language=es-ES`)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
             conteinerMovi.innerHTML = `
-            <div class="conteianr">
-                <h1>Titulo: ${data.original_title}</h1>
-                <p>Detalle: ${data.overview}</p>
-                <p>Puntuaciòn: ${data.popularity}</p>
+            <div class="conteianr text-center">
+                <h1>Titulo original: ${data.original_title} </h1>
                 <img src="${imageURL + data.poster_path}" alt="poster_img_${
                 data.original_title
             }">
-            </div>`;
+                <button type="button" class="btn btn-primary m-4 d-block" data-bs-toggle="modal" data-bs-target="#modalMovi">Más detalles</button>
+            </div>
+
+                <div class="modal fade" id="modalMovi" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitleId">${
+                        data.original_title
+                    }</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Detalle: ${data.overview}</p>
+                    <p>Puntuaciòn: ${data.popularity}</p>
+                </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+            `;
         });
 }
 // NOTE - Funcion para cambiar de tv
-function cargarMasTV() {
+function MoreTV() {
     tv_id++;
     CargarTV(tv_id);
 }
@@ -43,17 +63,34 @@ function CargarTV(i) {
         .then((datatv) => {
             console.log(datatv);
             conteinerTV.innerHTML = `
-                        <div class="conteianr">
+                        <div class="conteianr text-center">
                             <h1>Titulo: ${datatv.original_name}</h1>
-                            <p>Ultimo episodio: ${datatv.last_air_date}</p>
-                            <p>Puntuaciòn: ${datatv.popularity}</p>
-                            <p>Lenguaje original: ${
-                                datatv.original_language
-                            }</p>
-                            <p>Genoro: ${datatv.genres[0].name}</p>
                             <img src="${
                                 imageURL + datatv.poster_path
                             }" alt="poster_img_${datatv.original_name}">
-                        </div>`;
+                            <button type="button" class="btn btn-primary m-4 d-block" data-bs-toggle="modal" data-bs-target="#modalTV">Más detalles</button>            
+                </div>
+                <div class="modal fade" id="modalTV" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitleId">${
+                        datatv.original_name
+                    }</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>ultimo capitulo: ${datatv.last_air_date}</p>
+                    <p>Puntuaciòn: ${datatv.popularity}</p>
+                    <p>lenguaje original: ${datatv.original_language}</p>
+                    <p>Genero: ${datatv.genres[0].name}</p>
+                </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+                `;
         });
 }
